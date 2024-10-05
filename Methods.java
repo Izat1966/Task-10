52bad788af528a8663ecdd14735b395659b32b3e
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2024-10-05 17:13:02 +0600

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -1,4 +1,23 @@
+public class StringProcessor {
+    private String password;
+    private String sentence;
+    private String expression;
+    public int CalculateDigits(String sentence) {
+        int c = 0;
+        for (int i=0; i<sentence.length(); i++) {
+            char a = sentence.charAt(i);
+            if (Character.isDigit(a)) {
+                c++;
+            }
+        }
+        return c;
+    }
+  
+    public String GetSentence() {
+        return sentence;
+    }
 
-
-public class Methods {
+    public StringProcessor() {
+        sentence = "This sentence";
+    }
 }
